a0bae2870e1a2de6700184a4ce1f08c77f9b113a
Matheus Afonso <94645126+Matheus-A-Afonso@users.noreply.github.com>
2022-01-29 17:50:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -464,6 +464,23 @@ if (numero >=0 && numero <=5) {
     console.log('o numero está entre 0 e 11');
 }
 
+//////////////////////////////////////////////////////////////////////////////////
+
+// operadores de comparação
+/*
+> maior que
+>= maior que ou igual bolean
+< menor que
+<= menor que ou igual
+== igualdade (valor) não se usa
+=== igualdade estrita (valor e tipo)
+!= diferente não usa
+!== diferente estrito (valor e tipo)
+ */
+const num1 = 10;
+const num2 = 10;
+const comp = num1 === num2;
+console.log(comp);
 
 
 
